8abd06c9093f7c6542d0005448b6f07674fac1ca
IsamMakkie <69981037+IsamMakkie@users.noreply.github.com>
2020-08-20 12:26:00 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,22 @@
+/***********************
+* Submlime
+* Isam Makkie
+* 8/20/20
+* CMSC 255 - 001
+************************/
+
+
+ public class Hello {
+ 			public static void main(String[] args) {
+                
+             System.out.println("Isam Makkie");
+             System.out.println("LAB 1");
+             System.out.println("Downloading java and testing it");
+             System.out.println("8/20/20");
+             System.out.println("CMSC 255 - 001");
+             System.out.println();
+             }
+            
+
+        
+}
